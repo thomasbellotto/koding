d0b39650b097f791140e49a03ad4fd176a812cc3
Thomas Bellotto <thomasbellotto@yahoo.ca>
2015-10-22 22:07:50 +0000

    commit being completed

---
+++ app/models/has_gravatar.rb
@@ -0,0 +1,6 @@
+module HasGravatar
+
+    def gravatar 
+        "http://www.gravatar.com/avatar/#{Digest::MD5::hexdigest(email.downcase)}"
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  resources :answers
+  resources :questions
+  root 'home#index'
+  
+  get '/about' => 'home#about'
+ 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
